308daf384979cea86a3d7985577c0a754ada8d12
Stefan <euler90h@gmail.com>
2015-07-27 18:41:02 -0400

    added pt-sub, for subtracting two points

--- point.rkt
+++ point.rkt
@@ -1,12 +1,16 @@
 #lang racket
 
-(provide (struct-out pt) pt-add pt-mag)
+(provide (struct-out pt) pt-add pt-mag pt-sub)
 
 (struct pt (x y) #:transparent)
 
 (define (pt-add p q)
 	(pt (+ (pt-x p) (pt-x q))
 		(+ (pt-y p) (pt-y q))))
+
+(define (pt-sub p q)
+	(pt (- (pt-x p) (pt-x q))
+		(- (pt-y p) (pt-y q))))
 
 (define (pt-mag p)
 	(sqrt (+ (sqr (pt-x p)) (sqr (pt-y p)))))
