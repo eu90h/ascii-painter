f46fca3e2e64049a85c02562358e699952919667
Stefan <euler90h@gmail.com>
2015-07-27 21:40:07 -0400

    limited history to 2000 entries

--- history.rkt
+++ history.rkt
@@ -6,9 +6,13 @@
 
 (define history null)
 
+(define HISTORY-MAX 2000) ; max number of items in history
+(define HISTORY-DROP 500) ; how many actions to remove from the history after going over HISTORY-MAX
+
 (define (set-and-add-to-history scene x y tile)
-  (set! history (append history (list (list (send scene get x y) x y))))
-  (send scene set x y tile))
+	(when (>= (length history) HISTORY-MAX) (drop history HISTORY-DROP))
+  	(set! history (append history (list (list (send scene get x y) x y))))
+  	(send scene set x y tile))
 
 (define (undo-last-action scene)
 	(unless (zero? (length history))
