7291639ff4d7f4a84de9ab5cfc6abb5789060245
Stefan <euler90h@gmail.com>
2015-07-27 22:46:30 -0400

    fixed bug related to undoing lines not clearing the last history element

--- history.rkt
+++ history.rkt
@@ -21,9 +21,7 @@
 
 (define (undo-paint-action scene action-datum)
 	(let* ([t (first action-datum)] [x (second action-datum)] [y (third action-datum)])
-		(send scene set x y t)
-    	(if (<= (length history) 1) (set! history null)
-      		(set! history (take history (sub1 (length history)))))))
+		(send scene set x y t)))
 
 (define (undo-line-action scene action-data)
 	(map ((curry undo-paint-action) scene) action-data))
@@ -33,4 +31,6 @@
 		(let ([last-action (last history)])
 			(case (action-type last-action)
 				[(paint) (undo-paint-action scene (first (action-change-data last-action)))]
-				[(line) (undo-line-action scene (action-change-data last-action))]))))
+				[(line) (undo-line-action scene (action-change-data last-action))])
+			(if (<= (length history) 1) (set! history null)
+      		(set! history (take history (sub1 (length history))))))))
