7eae1170da30386b118e0a87bcaa6f5843fe89a7
Stefan <euler90h@gmail.com>
2015-07-28 23:29:40 -0400

    added function signatures to util

--- util.rkt
+++ util.rkt
@@ -7,18 +7,24 @@
 (define colors (send the-color-database get-names)) ; a list of color name strings
 
 ; List -> Any
+; returns a random element of a list
 (define (random-element list) (list-ref list (random (length list))))
 
 ; Void -> Color%
+; returns a random color object from the Racket color database
 (define (get-random-color) (send the-color-database find-color (random-element colors)))
 
-; Void -> Chars
+; Void -> Char
+; returns a random cp437 character
 (define (get-random-symbol) (string->symbol (random-element cp437-strings)))
 
+; Canvas% Event -> Void
+; takes a mouse event and clamps the mouse event coordinates to the given canvas
 (define (evt-clamp canvas evt)
 	(send canvas clamp (send evt get-x) (send evt get-y)))
 
 ; (Integer Integer -> Void) Pt Pt -> Void 
+; an implementation of Bresenham's line algorithm
 (define (trace-line callback p q)
   (define dx (abs (- (pt-x q) (pt-x p))))
   (define dy (abs (- (pt-y q) (pt-y p))))
@@ -29,7 +35,7 @@
     (when (eq? #f (member (list x y) the-pts))
       (callback x y)
       (set! the-pts (append the-pts (list (list x y))))))
- (define (trace)
+  (define (trace)
      (let ([error 0.0] [delta-error (if (zero? dx) 0.0 (abs (/ dy dx)))] [y-step (sign (- (pt-y q) (pt-y p)))])
       (define (loop x y0 e0)
       (unless (= (pt-x q) x)
@@ -46,13 +52,15 @@
     [(zero? dy) (trace-row apply-callback (pt-y p) (pt-x p) (pt-x q))]
     [else (trace)]))
 
-; (Integer Integer -> Void) Integer Integer Integer -> Void	
+; (Integer Integer -> Void) Integer Integer Integer -> Void
+; applies a callback to every point on a vertical line
 (define (trace-column callback x y0 y1)
 	(let* ([diff (- y1 y0)] [fact (if (< 0 diff) -1 1)])
 		(for ([i (in-range (abs diff))])
 			(callback x (+ y1 (* i fact))))))
 
 ; (Integer Integer -> Void) Integer Integer Integer -> Void
+; applies a callback to every point on a horizontal line
 (define (trace-row callback y x0 x1)
 	(let* ([diff (- x1 x0)] [fact (if (< 0 diff) -1 1)])
 		(for ([i (in-range (abs diff))])
